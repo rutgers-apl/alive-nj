838278ec01eb14b033e6ca3f36e08532fbeb17b3
David Menendez <zednenem@psualum.com>
2016-02-01 23:34:29 -0500

    New configuration and logging setup
    * --quiet and --persist options
    * config.py stores default configuration parameters
    * logs in log/
    * log/empty only exists to make git happy

--- alive.py
+++ alive.py
@@ -1,34 +1,54 @@
 #! /usr/bin/env python
 
+import logging, logging.config, argparse, sys
+import config
 from parser import parse_opt_file
-import logging, argparse, sys
 
-def check_opt(opt):
-  print '----------'
-  print opt.format()
-  print
+def check_opt(opt, chatty=True):
+  if chatty:
+    print '----------'
+    print opt.format()
+    print
   
   proofs = 0
   for m in opt.z3_models():
     r = opt.check_refinement(m)
     if r:
-      print r
-      exit(-1)
+      if chatty:
+        print '\nOptimization is incorrect!'
+        print r
+      return False
     
     proofs += 1
-    sys.stdout.write('\rProofs: ' + str(proofs))
-    sys.stdout.flush()
+    if chatty and sys.stdout.isatty():
+      sys.stdout.write('\rDone: ' + str(proofs))
+      sys.stdout.flush()
   
-  print
+  if chatty:
+    print '\nOptimization is correct'
+
+  return True
 
 def main():
+  logging.config.dictConfig(config.logs)
+
   parser = argparse.ArgumentParser()
+  parser.add_argument('--persist', action='store_true',
+    default=config.persist,
+    help='continue processing opts after verification failures')
+  parser.add_argument('--quiet', action='store_true',
+    default=config.quiet,
+    help='only print number of tested and unverified opts')
   parser.add_argument('file', type=argparse.FileType('r'), nargs='*',
     default=[sys.stdin],
     help='file(s) containing Alive optimizations (stdin by default')
 
   args = parser.parse_args()
-  
+
+  chatty = not args.quiet
+  tested = 0
+  failed = 0
+
   for f in args.file:
     if f.isatty():
       sys.stderr.write('[Reading from terminal...]\n')
@@ -36,11 +56,27 @@ def main():
     opts = parse_opt_file(f.read())
     
     for opt in opts:
-      check_opt(opt)
+      tested += 1
+      if not check_opt(opt, chatty):
+        failed += 1
+        if not args.persist:
+          exit(-1)
+      if not chatty and sys.stdout.isatty():
+        sys.stdout.write('\rOpts tested: {}; Failures: {}'.format(
+          tested, failed))
+        sys.stdout.flush()
+
+  if chatty or not sys.stdout.isatty():
+    print 'Opts tested: {}; Failures: {}'.format(tested, failed)
+  else:
+    print
 
 if __name__ == '__main__':
-  logging.basicConfig()
   try:
     main()
   except KeyboardInterrupt:
-    print '\nExiting\n'
+    sys.stderr.write('\n[Keyboard interrupt]\n')
+  except Exception, e:
+    logging.exception(e)
+    print '\nExiting with exception (see log)'
+    exit(-1)
---
+++ config.py
@@ -0,0 +1,54 @@
+'''
+Central location for global configuration parameters.
+'''
+import sys
+
+# continue after failure
+persist = False
+
+# minimal output
+quiet = False
+
+# logging configuration. see logging.config docs for schema
+logs = {
+  'version': 1,
+  'disable_existing_loggers': False,
+  
+  'formatters': {
+    'default': {
+      'format': '%(asctime)s - %(levelname)-8s - %(name)s - %(message)s',
+    },
+    
+    'brief': {
+      'format': '%(message)s'
+    }
+  },
+  
+  'handlers': {
+    'default': {
+      'class': 'logging.handlers.RotatingFileHandler',
+      'formatter': 'default',
+      'filename': 'log/alive.log',
+      'maxBytes': 1024000,
+      'backupCount': 5,
+    },
+    
+    'warnings': {
+      'class': 'logging.StreamHandler',
+      'formatter': 'brief',
+      'level': 'WARNING',
+      'stream': sys.stderr,
+    }
+  },
+  
+  'root': {
+    'level': 'INFO',
+    'handlers': ['default', 'warnings']
+  },
+  
+  'loggers': {
+    'smtinterp': {
+      'level': 'DEBUG',
+    }
+  }
+}
