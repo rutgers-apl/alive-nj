8f2e3fbf7bd16b5392cbf73a9d6c2c7241ada670
David Menendez <zednenem@psualum.com>
2016-08-19 00:13:15 -0400

    Enumerator improvements
    Generate literals sooner, but still after symbolic constants.
    Generate predicates later.
    Generate log2(). TODO: reduce wasted work when generating log2().

--- alive/precondition/enumerator.py
+++ alive/precondition/enumerator.py
@@ -10,8 +10,12 @@ def set_type(term, tyvar):
   typing.context[term] = tyvar
   return term
 
-binops = L.BinaryCnxp.codes.values()
+#binops = L.BinaryCnxp.codes.values()
+binops = (L.AddCnxp, L.SubCnxp, L.AndCnxp, L.OrCnxp, L.XorCnxp, L.MulCnxp,
+          L.ShlCnxp, L.SDivCnxp, L.UDivCnxp, L.SRemCnxp, L.URemCnxp, L.LShrCnxp,
+          L.AShrCnxp)
 unops = L.UnaryCnxp.codes.values()
+
 
 def expressions(size, tyvar, config):
   """Generate pairs (expr, bool) containing expressions of a particular size
@@ -27,15 +31,17 @@ def expressions(size, tyvar, config):
     for s in config.symbols:
       if typing.context[s] == tyvar:
         yield s, True
-
-    for r in config.type_reps:
-      yield set_type(L.WidthCnxp(r), tyvar), tyvar == config.model.default_id
-    return
-
-  if size == 2:
+#     return
+#
+#   if size == 2:
     yield set_type(L.Literal(0), tyvar), False
     yield set_type(L.Literal(1), tyvar), False
     yield set_type(L.Literal(-1), tyvar), False
+
+    for r in config.type_reps:
+      yield set_type(L.WidthCnxp(r), tyvar), tyvar == config.model.default_id
+
+    return
 
   size -= 1
   for lsize in xrange(1,size):
@@ -47,9 +53,22 @@ def expressions(size, tyvar, config):
   for op in unops:
     for e,s in expressions(size, tyvar, config):
       if not s: continue
+      if isinstance(e, L.UnaryCnxp): continue
       yield set_type(op(e), tyvar), s
 
   # TODO: functions
+  size =- 1
+  for e,s in expressions(size, tyvar, config):
+    if not s: continue
+    yield set_type(L.AbsCnxp(e), tyvar), s
+
+  # FIXME: precompute!
+  tyvars = { typing.context[s] for s in config.symbols }
+  assert config.model.default_id not in tyvars
+  for t2 in tyvars:
+    for e,s in expressions(size, t2, config):
+      if not s: continue
+      yield set_type(L.Log2Cnxp(e), tyvar), tyvar == config.model.default_id
 
 def predicates(size, config):
   tyvars = { typing.context[s] for s in config.symbols }
@@ -63,12 +82,12 @@ def predicates(size, config):
           if not (s1 or s2): continue
 
           # TODO: avoid ult if tyvar is floating
-          for cmp in ['eq', 'ult', 'slt', 'ugt', 'sgt']: # sufficient?
+          for cmp in ['eq', 'ult', 'ugt', 'slt', 'sgt']: # sufficient?
             yield L.Comparison(cmp, l, r)
 
   # enumerate unary predicates
   for tyvar in tyvars:
-    for e,s in expressions(size-2, tyvar, config):
+    for e,s in expressions(size-3, tyvar, config):
       if not s: continue
 
       yield L.IntMinPred(e)
